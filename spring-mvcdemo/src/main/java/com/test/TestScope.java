8cd6182bb69da3cf37cf5dc65949fdb2aacec88c
weiwei <weiwei.ms@gmail.com>
2018-04-25 12:38:10 +0800

    TestScope加载上下文方式变更

--- spring-mvcdemo/src/main/java/com/test/TestScope.java
+++ spring-mvcdemo/src/main/java/com/test/TestScope.java
@@ -3,7 +3,7 @@ package com.test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Date;
 
@@ -12,8 +12,9 @@ public class TestScope {
     private static Logger logger = LoggerFactory.getLogger(TestScope.class);
 
     public static void main(String[] args) {
-        ApplicationContext context = new FileSystemXmlApplicationContext(
-                "classpath:web/applicationContext.xml");
+        // ApplicationContext context = new FileSystemXmlApplicationContext(
+        //         "spring-mvcdemo/src/main/webapp/WEB-INF/applicationContext.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("web/applicationContext.xml");
 
         Date date1 = (Date) context.getBean("singleton");
         Date date2 = (Date) context.getBean("prototype");
@@ -23,5 +24,4 @@ public class TestScope {
         logger.info("========== singleton: {}", date1 == date3);
         logger.info("========== prototype: {}", date2 == date4);
     }
-
 }
