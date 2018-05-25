691253d5934587e96d96ee420d9a58b8e1867652
weiwei <weiwei.ms@gmail.com>
2018-05-25 17:50:00 +0800

    可重入自旋锁

---
+++ spring-concurrent/src/main/java/com/weiwei/lock/ReentrantSpinLock.java
@@ -0,0 +1,32 @@
+package com.weiwei.lock;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * 可重入自旋锁
+ */
+public class ReentrantSpinLock {
+    private AtomicReference<Thread> owner = new AtomicReference<>();
+    private int count = 0;
+
+    public void lock() {
+        Thread current = Thread.currentThread();
+        if (current == owner.get()) {
+            count++;
+            return;
+        }
+        while (!owner.compareAndSet(null, current)) {
+        }
+    }
+
+    public void unlock() {
+        Thread current = Thread.currentThread();
+        if (current == owner.get()) {
+            if (count != 0) {
+                count--;
+            } else {
+                owner.compareAndSet(current, null);
+            }
+        }
+    }
+}
