b5dc521bb9554b9495e70874dc320f5a2d09f9fe
weiwei <weiwei.ms@gmail.com>
2018-08-03 15:12:55 +0800

    Netty时间服务器实现

---
+++ spring-nettydemo/src/main/java/com/weiwei/timeServer/netty/TimeServer.java
@@ -0,0 +1,56 @@
+package com.weiwei.timeServer.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimeServer {
+
+    public void bind(int port) throws Exception {
+        // 配置服务端的NIO线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+            // 绑定端口，同步等待成功
+            ChannelFuture future = bootstrap.bind(port).sync();
+
+            // 等待服务端监听端口关闭
+            future.channel().closeFuture().sync();
+        } finally {
+            // 释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel ch) throws Exception {
+            ch.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+        }
+
+        new TimeServer().bind(port);
+    }
+}
