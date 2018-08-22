a9a93d46fb116af4834cb2e8ec9cb06c8119bb3a
weiwei <weiwei.ms@gmail.com>
2018-08-22 14:38:20 +0800

    通过JavaConfig装配Bean

--- spring-mvcdemo/src/main/java/com/weiwei/config/CDPlayerConfig.java
+++ spring-mvcdemo/src/main/java/com/weiwei/config/CDPlayerConfig.java
@@ -1,9 +1,21 @@
 package com.weiwei.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import com.weiwei.soundsystem.CDPlayer;
+import com.weiwei.soundsystem.CompactDisc;
+import com.weiwei.soundsystem.SgtPeppers;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.weiwei.soundsystem")
+// @ComponentScan(basePackages = "com.weiwei.soundsystem")
 public class CDPlayerConfig {
+    @Bean(name = "lonelyHeart")
+    public CompactDisc sgtPeppers() {
+        return new SgtPeppers();
+    }
+
+    @Bean
+    public CDPlayer cdPlayer(CompactDisc compactDisc) {
+        return new CDPlayer(compactDisc);
+    }
 }
