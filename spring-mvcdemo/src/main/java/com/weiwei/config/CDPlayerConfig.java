30fbaa07477a389d46a92388958e80c030e29dbf
weiwei <weiwei.ms@gmail.com>
2018-09-28 22:18:50 +0800

    @ComponentScan注解需和@Component等注解搭配实现组件扫描；得到的Bean由@Autowired注解实现自动装配

--- spring-mvcdemo/src/main/java/com/weiwei/config/CDPlayerConfig.java
+++ spring-mvcdemo/src/main/java/com/weiwei/config/CDPlayerConfig.java
@@ -1,21 +1,19 @@
 package com.weiwei.config;
 
-import com.weiwei.soundsystem.CDPlayer;
-import com.weiwei.soundsystem.CompactDisc;
-import com.weiwei.soundsystem.SgtPeppers;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-// @ComponentScan(basePackages = "com.weiwei.soundsystem")
+@ComponentScan(basePackages = "com.weiwei.soundsystem")
 public class CDPlayerConfig {
-    @Bean(name = "lonelyHeart")
-    public CompactDisc sgtPeppers() {
-        return new SgtPeppers();
-    }
 
-    @Bean
-    public CDPlayer cdPlayer(CompactDisc compactDisc) {
-        return new CDPlayer(compactDisc);
-    }
+    // @Bean(name = "lonelyHeart")
+    // public CompactDisc sgtPeppers() {
+    //     return new SgtPeppers();
+    // }
+    //
+    // @Bean
+    // public MediaPlayer cdPlayer(CompactDisc compactDisc) {
+    //     return new CDPlayer(compactDisc);
+    // }
 }
