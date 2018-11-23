0d51b0b3589c1f642ac67667f776dd02928215a8
weiwei <weiwei.ms@gmail.com>
2018-11-23 19:20:35 +0800

    修改

---
+++ spring-mvcproject/src/main/java/com/weiwei/test/OverrideTest.java
@@ -0,0 +1,37 @@
+package com.weiwei.test;
+
+public class OverrideTest {
+    public static void main(String[] args) {
+        T t = new B();
+        t.foo();
+    }
+}
+
+class T {
+    T() {
+        System.out.println("T()");
+    }
+
+    void foo() {
+        // this代表"当前对象"
+        this.bar();
+    }
+
+    void bar() {
+        System.out.println("T.bar");
+    }
+}
+
+class B extends T {
+    B() {
+        System.out.println("B()");
+    }
+
+    void foo() {
+        super.foo();
+    }
+
+    void bar() {
+        System.out.println("B.bar");
+    }
+}
